89b9dacc8c4ec5c6d34e7a9494b504e090c6c944
Deepak kumar <yadavkd9955@gmail.com>
2025-06-24 06:49:53 +0530

    This Commit in this App Navigation Add

--- app/src/main/java/com/example/tezora/view/Home.kt
+++ app/src/main/java/com/example/tezora/view/Home.kt
@@ -4,10 +4,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -18,7 +16,7 @@ import androidx.compose.ui.unit.sp
 @Preview(showSystemUi = true)
 fun Home() {
 
-   
+
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = {
